6e4fa27fcd1fdee55648fa6c7d8875f845b29173
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2019-05-28 18:29:14 +0300

    Implement exercise 1.1.7
    Give the value printed by each of the following code fragments:
    a. double t = 9.0;
       while (Math.abs(t - 9.0/t) > .001)
           t = (9.0/t + t) / 2.0;
       StdOut.printf("%.5f\n", t);

--- algorithms/princeton-university/chapter-1-fundamentals/src/test/java/my/rinat/chapter1/Exercise1.java
+++ algorithms/princeton-university/chapter-1-fundamentals/src/test/java/my/rinat/chapter1/Exercise1.java
@@ -84,4 +84,13 @@ class Exercise1 {
             g = f - g;
         }
     }
+
+    @Test
+    void exercise_1_1_7_a() {
+        double t = 9.0;
+        while (Math.abs(t - 9.0 / t) > .001) {
+            t = (9.0 / t + t) / 2.0;
+        }
+        StdOut.printf("%.5f\n", t);
+    }
 }
