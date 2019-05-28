a06f76ebad785288824a91125a36f0b9787a2aec
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2019-05-28 18:38:15 +0300

    Implement exercise 1.1.12
    What does the following code fragment print?
        int[] a = new int[10];
        for (int i = 0; i < 10; i++)
            a[i] = 9 - i;
        for (int i = 0; i < 10; i++)
            a[i] = a[a[i]];
        for (int i = 0; i < 10; i++)
            System.out.println(a[i]);

--- algorithms/princeton-university/chapter-1-fundamentals/src/test/java/my/rinat/chapter1/Exercise1.java
+++ algorithms/princeton-university/chapter-1-fundamentals/src/test/java/my/rinat/chapter1/Exercise1.java
@@ -157,4 +157,18 @@ class Exercise1 {
             StdOut.println();
         }
     }
+
+    @Test
+    void exercise_1_1_12() {
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++) {
+            a[i] = 9 - i;
+        }
+        for (int i = 0; i < 10; i++) {
+            a[i] = a[a[i]];
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a[i]);
+        }
+    }
 }
