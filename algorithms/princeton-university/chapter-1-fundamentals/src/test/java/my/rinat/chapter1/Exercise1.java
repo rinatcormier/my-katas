5ed2e357a72321cf7761a6cbd825a0b87377d864
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2019-05-30 16:48:11 +0300

    Implement exercise 1.1.18
    Consider the following recursive function:
    public static int mystery(int a, int b)
    {
        if (b == 0) return 0;
        if (b % 2 == 0) return mystery(a+a, b/2);
        return mystery(a+a, b/2) + a;
    }
    What are the values of mystery(2, 25) and mystery(3, 11)? Given positive
    integers a and b, describe what value mystery(a, b) computes. Answer
    the same question, but replace + with * and replace return 0 with
    return 1.

--- algorithms/princeton-university/chapter-1-fundamentals/src/test/java/my/rinat/chapter1/Exercise1.java
+++ algorithms/princeton-university/chapter-1-fundamentals/src/test/java/my/rinat/chapter1/Exercise1.java
@@ -273,4 +273,20 @@ class Exercise1 {
         }
         return exR1(n - 3) + n + exR1(n - 2) + n;
     }
+
+    @Test
+    void exercise_1_1_18() {
+        assertThat(mystery(2, 25)).isEqualTo(50);
+        assertThat(mystery(3, 11)).isEqualTo(33);
+    }
+
+    private static int mystery(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+        if (b % 2 == 0) {
+            return mystery(a + a, b / 2);
+        }
+        return mystery(a + a, b / 2) + a;
+    }
 }
