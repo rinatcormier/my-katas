a3ebf1f565748c30746bc72f84c35a3fc634957f
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2019-05-30 15:19:52 +0300

    Implement exercise 1.1.16
    Give the value of exR1(6) :
    public static String exR1(int n)
    {
        if (n <= 0) return "";
        return exR1(n-3) + n + exR1(n-2) + n;
    }

--- algorithms/princeton-university/chapter-1-fundamentals/src/test/java/my/rinat/chapter1/Exercise1.java
+++ algorithms/princeton-university/chapter-1-fundamentals/src/test/java/my/rinat/chapter1/Exercise1.java
@@ -261,4 +261,16 @@ class Exercise1 {
                 Arguments.of(new int[] {1, 2, 2, 3, 3, 3}, 5, new int[] {0, 1, 2, 3, 0})
         );
     }
+
+    @Test
+    void exercise_1_1_16() {
+        StdOut.println(exR1(6));
+    }
+
+    private static String exR1(int n) {
+        if (n <= 0) {
+            return "";
+        }
+        return exR1(n - 3) + n + exR1(n - 2) + n;
+    }
 }
