a89d4d2386c1ae4795845aee80a1563e278a3228
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2019-07-11 08:42:10 +0300

    Solve "a+b" issue

---
+++ algorithms/stepik/theory-and-practice_methods/src/main/java/org/stepik/part1/Main.java
@@ -0,0 +1,13 @@
+package org.stepik.part1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
