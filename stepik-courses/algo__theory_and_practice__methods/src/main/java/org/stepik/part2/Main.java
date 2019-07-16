5ef368357fa9f7611682d91d70ccda541db28bd3
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2019-07-16 14:22:25 +0300

    Test chapter 2.2 exercise 1
    Course: https://stepik.org/course/217

---
+++ stepik-courses/algo__theory_and_practice__methods/src/main/java/org/stepik/part2/Main.java
@@ -0,0 +1,28 @@
+package org.stepik.part2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println(fib(in.nextInt()));
+        }
+    }
+
+    private static long fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        long[] fibs = new long[n + 1];
+        fibs[0] = 0;
+        fibs[1] = 1L;
+        for (int i = 2; i <= n; i++) {
+            fibs[i] = fibs[i - 1] + fibs[i - 2];
+        }
+        return fibs[n];
+    }
+}
