902b50270ffab650798ccb018936267fc460c9fb
Rinat <rinat.zalyaletdinov@yandex.com>
2025-04-01 15:57:53 +0300

    Implement one of the tasks

--- stepik-courses/stepik-python-programming-course/hello.py
+++ stepik-courses/stepik-python-programming-course/hello.py
@@ -1,6 +1,11 @@
-x = int(input())
-h = int(input())
-m = int(input())
-s = x + (h * 60) + m
-print(s // 60)
-print(s % 60)
+n = int(input())
+a = n // 100000
+b = n // 10000 % 10
+c = n // 1000 % 10
+d = n // 100 % 10
+e = n // 10 % 10
+f = n % 10
+if (a + b + c) == (d + e + f):
+    print('Счастливый')
+else:
+    print('Обычный')
