f77f0e09b2fc598d96344cc061e3b1eb370ede24
Rinat <rinat.zalyaletdinov@yandex.com>
2025-04-11 09:35:44 +0300

    Implement one more task

--- stepik-courses/stepik-python-programming-course/hello.py
+++ stepik-courses/stepik-python-programming-course/hello.py
@@ -1,13 +1,10 @@
-s = input()
-current = s[0]
-counter = 0
-result = ''
-for c in s:
-    if current == c:
-        counter += 1
-    else:
-        result += (current + str(counter))
-        current = c
-        counter = 1
-result += (current + str(counter))
-print(result)
+a = [int(i) for i in (input().split())]
+if len(a) == 1:
+    print(a[0])
+    exit(0)
+b = []
+a.insert(0, a[len(a) - 1])
+a.append(a[1])
+for i in range(1, len(a) - 1):
+    b.append(a[i - 1] + a[i + 1])
+print(' '.join([str(i) for i in b]))
