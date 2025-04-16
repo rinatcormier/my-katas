6ce84f1f2d4114e2cb84034274d63aa8784b84ba
Rinat <rinat.zalyaletdinov@yandex.com>
2025-04-16 10:19:02 +0300

    Try an example in Python

---
+++ stepik-courses/stepik-python-programming-course/ex.py
@@ -0,0 +1,11 @@
+dna = 'ATTCGGAGCT'
+print('Original string:', dna)
+print(dna[1])
+print(dna[1:4])
+print(dna[:4])
+print(dna[4:])
+print(dna[-4:])
+print(dna[1:-1])
+print(dna[1:-1:2])
+print(dna[::-1])
+
