75155502353c8e1610f32dd4553d8eda95b7f65f
Rinat <rinat.zalyaletdinov@yandex.com>
2025-06-02 18:33:27 +0300

    Implement algorithm with matrix in python

--- stepik-courses/stepik-python-programming-course/ex.py
+++ stepik-courses/stepik-python-programming-course/ex.py
@@ -1,11 +1,37 @@
-dna = 'ATTCGGAGCT'
-print('Original string:', dna)
-print(dna[1])
-print(dna[1:4])
-print(dna[:4])
-print(dna[4:])
-print(dna[-4:])
-print(dna[1:-1])
-print(dna[1:-1:2])
-print(dna[::-1])
+line = input()
+matrix = list()
+while line != 'end':
+    matrix.append([int(i) for i in (line.split())])
+    line = input()
+result = list()
+for i in range(0, len(matrix)):
+    result.append(list())
+    for j in range(0, len(matrix[i])):
+        s = 0
 
+        if j == 0:
+            s += matrix[i][len(matrix[j]) -1]
+        else:
+            s += matrix[i][j - 1]
+
+        if i == 0:
+            s += matrix[len(matrix) - 1][j]
+        else:
+            s += matrix[i - 1][j]
+
+        if j == len(matrix[i]) - 1:
+            s += matrix[i][0]
+        else:
+            s += matrix[i][j + 1]
+
+        if i == len(matrix) - 1:
+            s += matrix[0][j]
+        else:
+            s += matrix[i + 1][j]
+
+        result[i].append(s)
+
+for i in range(0, len(result)):
+    for j in range(0, len(result[i])):
+        print(result[i][j], end=' ')
+    print()
