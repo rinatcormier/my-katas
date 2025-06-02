53a1df8d1a81aca8740fd73015d8b67b7cecef51
Rinat <rinat.zalyaletdinov@yandex.com>
2025-06-02 18:40:17 +0300

    Implement "Ten minute walk" kata

--- codewars/src/main/java/my/rinat/codewars/tenminuteswalk/TenMinWalk.java
+++ codewars/src/main/java/my/rinat/codewars/tenminuteswalk/TenMinWalk.java
@@ -1,5 +1,6 @@
 package my.rinat.codewars.tenminuteswalk;
 
+// https://www.codewars.com/kata/54da539698b8a2ad76000228/train/java
 public class TenMinWalk {
     public static boolean isValid(char[] walk) {
         return true;
