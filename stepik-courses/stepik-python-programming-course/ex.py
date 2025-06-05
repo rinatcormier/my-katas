d90655dda6e4af4f29507bccf87a15191d11bf93
Rinat <rinat.zalyaletdinov@yandex.com>
2025-06-05 16:45:09 +0300

    Задача со степика

--- stepik-courses/stepik-python-programming-course/ex.py
+++ stepik-courses/stepik-python-programming-course/ex.py
@@ -1,37 +1,28 @@
-line = input()
-matrix = list()
-while line != 'end':
-    matrix.append([int(i) for i in (line.split())])
-    line = input()
-result = list()
-for i in range(0, len(matrix)):
-    result.append(list())
-    for j in range(0, len(matrix[i])):
-        s = 0
+# https://stepik.org/lesson/3369/step/11?unit=952
+n = int(input())
 
-        if j == 0:
-            s += matrix[i][len(matrix[j]) -1]
-        else:
-            s += matrix[i][j - 1]
+if n == 1:
+    print("1")
 
-        if i == 0:
-            s += matrix[len(matrix) - 1][j]
-        else:
-            s += matrix[i - 1][j]
+if n == 2:
+    print("1 2")
+    print("4 3")
 
-        if j == len(matrix[i]) - 1:
-            s += matrix[i][0]
-        else:
-            s += matrix[i][j + 1]
-
-        if i == len(matrix) - 1:
-            s += matrix[0][j]
-        else:
-            s += matrix[i + 1][j]
-
-        result[i].append(s)
-
-for i in range(0, len(result)):
-    for j in range(0, len(result[i])):
-        print(result[i][j], end=' ')
-    print()
+spiral = [[0] * n for k in range(n)]
+if n > 2:
+    current = 1
+    for j in range(n):
+        spiral[0][j] = current
+        current += 1
+    for i in range(1, n):
+        spiral[i][n - 1] = current
+        current += 1
+    for j in range(n - 2, 0, -1):
+        spiral[n - 1][j] = current
+        current += 1
+    last = n * n + 1
+    i = n - 1
+    j = 0
+    direction = "up"
+    while current < last:
+        pass # todo
