5bca2f3838ba20eb44bc713a53340ac0420e2476
Rinat <rinat.zalyaletdinov@yandex.com>
2025-06-09 17:23:59 +0300

    Задача со степика: двумерный массив - спираль

--- stepik-courses/stepik-python-programming-course/ex.py
+++ stepik-courses/stepik-python-programming-course/ex.py
@@ -1,28 +1,45 @@
-# https://stepik.org/lesson/3369/step/11?unit=952
 n = int(input())
 
-if n == 1:
-    print("1")
+spiral = [[0] * n for _ in range(n)]
+direction = "right"
+currentValue = 1
+lastValue = n ** 2
+i, j = 0, 0
+while currentValue <= lastValue:
+    if direction == "right":
+        while j < n and spiral[i][j] == 0:
+            spiral[i][j] = currentValue
+            j += 1
+            currentValue += 1
+        i += 1
+        j -= 1
+        direction = "down"
+    elif direction == "down":
+        while i < n and spiral[i][j] == 0:
+            spiral[i][j] = currentValue
+            i += 1
+            currentValue += 1
+        i -= 1
+        j -= 1
+        direction = "left"
+    elif direction == "left":
+        while j >= 0 and spiral[i][j] == 0:
+            spiral[i][j] = currentValue
+            j -= 1
+            currentValue += 1
+        i -= 1
+        j += 1
+        direction = "up"
+    elif direction == "up":
+        while i >= 0 and spiral[i][j] == 0:
+            spiral[i][j] = currentValue
+            i -= 1
+            currentValue += 1
+        i += 1
+        j += 1
+        direction = "right"
 
-if n == 2:
-    print("1 2")
-    print("4 3")
-
-spiral = [[0] * n for k in range(n)]
-if n > 2:
-    current = 1
-    for j in range(n):
-        spiral[0][j] = current
-        current += 1
-    for i in range(1, n):
-        spiral[i][n - 1] = current
-        current += 1
-    for j in range(n - 2, 0, -1):
-        spiral[n - 1][j] = current
-        current += 1
-    last = n * n + 1
-    i = n - 1
-    j = 0
-    direction = "up"
-    while current < last:
-        pass # todo
+for i in range(0, len(spiral)):
+    for j in range(0, len(spiral[i])):
+        print(spiral[i][j], end=' ')
+    print()
