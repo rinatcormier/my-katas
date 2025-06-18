ba3c26f69dfc7215db0ab4333b16ea7d7c2e7be7
Rinat <rinat.zalyaletdinov@yandex.com>
2025-06-18 15:57:30 +0300

    Задача на работу с файлами в python

--- stepik-courses/stepik-python-programming-course/ex.py
+++ stepik-courses/stepik-python-programming-course/ex.py
@@ -1,45 +1,18 @@
-n = int(input())
-
-spiral = [[0] * n for _ in range(n)]
-direction = "right"
-currentValue = 1
-lastValue = n ** 2
-i, j = 0, 0
-while currentValue <= lastValue:
-    if direction == "right":
-        while j < n and spiral[i][j] == 0:
-            spiral[i][j] = currentValue
-            j += 1
-            currentValue += 1
-        i += 1
-        j -= 1
-        direction = "down"
-    elif direction == "down":
-        while i < n and spiral[i][j] == 0:
-            spiral[i][j] = currentValue
-            i += 1
-            currentValue += 1
-        i -= 1
-        j -= 1
-        direction = "left"
-    elif direction == "left":
-        while j >= 0 and spiral[i][j] == 0:
-            spiral[i][j] = currentValue
-            j -= 1
-            currentValue += 1
-        i -= 1
-        j += 1
-        direction = "up"
-    elif direction == "up":
-        while i >= 0 and spiral[i][j] == 0:
-            spiral[i][j] = currentValue
-            i -= 1
-            currentValue += 1
-        i += 1
-        j += 1
-        direction = "right"
-
-for i in range(0, len(spiral)):
-    for j in range(0, len(spiral[i])):
-        print(spiral[i][j], end=' ')
-    print()
+with open("test.txt") as file:
+    words_counter = dict()
+    for line in file:
+        words = line.strip().lower().split()
+        for word in words:
+            if word not in words_counter:
+                words_counter[word] = 1
+            else:
+                words_counter[word] += 1
+    max_count = max(words_counter.values())
+    most_frequently_words = list()
+    for key, value in words_counter.items():
+        if value == max_count:
+            most_frequently_words.append(key + ":" + str(value))
+    most_frequently_words.sort()
+    for word_with_counter in most_frequently_words:
+        word, counter = word_with_counter.split(":")
+        print(word, counter)
