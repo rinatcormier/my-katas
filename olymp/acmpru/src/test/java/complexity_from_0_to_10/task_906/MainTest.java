b95d625df6831b26ab0a9f1f1d0f60ee77b79ace
Rinat <rinat.zalyaletdinov@yandex.com>
2025-09-11 10:50:06 +0300

    Add new module and resolve one task

---
+++ olymp/acmpru/src/test/java/complexity_from_0_to_10/task_906/MainTest.java
@@ -0,0 +1,37 @@
+package complexity_from_0_to_10.task_906;
+
+import complexity_from_0_to_10.util.MockedStdIO;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MainTest {
+
+    @ParameterizedTest
+    @MethodSource("candidates")
+    void testPrimesRange(String input, String expected) {
+        new MockedStdIO(input).run(output -> {
+            Main.main(new String[]{});
+            assertThat(output.toString()).isEqualTo(expected);
+        });
+    }
+
+    private static Stream<Arguments> candidates() {
+        return Stream.of(
+                Arguments.of(separatedObjectsAsString(1), separatedObjectsAsString(2)),
+                Arguments.of(separatedObjectsAsString(3), separatedObjectsAsString(4)),
+                Arguments.of(separatedObjectsAsString(1_000_000), separatedObjectsAsString(1_000_001))
+        );
+    }
+
+    private static String separatedObjectsAsString(Object... objects) {
+        return Stream.of(objects)
+                .map(String::valueOf)
+                .collect(joining(System.lineSeparator(), "", System.lineSeparator()));
+    }
+}
