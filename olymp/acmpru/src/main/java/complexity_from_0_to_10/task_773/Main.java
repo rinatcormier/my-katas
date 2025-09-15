59ee5a9b967f41cb82084016bdb7de831c78fc20
Rinat <rinat.zalyaletdinov@yandex.com>
2025-09-15 11:54:06 +0300

    Resolve acmp 773, refactoring

---
+++ olymp/acmpru/src/main/java/complexity_from_0_to_10/task_773/Main.java
@@ -0,0 +1,39 @@
+package complexity_from_0_to_10.task_773;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.function.BiConsumer;
+
+/**
+ * <a href="https://acmp.ru/index.asp?main=task&id_task=773">Задача</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Shell().execute((reader, writer) -> new Program(reader, writer).run());
+    }
+}
+
+class Shell {
+    void execute(BiConsumer<Scanner, PrintWriter> code) {
+        try (var scanner = new Scanner(System.in); var writer = new PrintWriter(System.out)) {
+            code.accept(scanner, writer);
+        }
+    }
+}
+
+class Program {
+
+    private final Scanner reader;
+    private final PrintWriter writer;
+
+    Program(Scanner reader, PrintWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    void run() {
+        final int k = reader.nextInt();
+        final int m = reader.nextInt();
+        writer.println(k * m * k);
+    }
+}
