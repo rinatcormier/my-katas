03246f0ef037057a82707dd99f987622d3a47a5f
Rinat <rinat.zalyaletdinov@yandex.com>
2025-09-23 13:25:00 +0300

    Resolve acmp 597

---
+++ olymp/acmpru/src/main/java/complexity_from_0_to_10/task_597/Main.java
@@ -0,0 +1,41 @@
+package complexity_from_0_to_10.task_597;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.function.BiConsumer;
+
+/**
+ * <a href="https://acmp.ru/index.asp?main=task&id_task=597">Задача</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Shell().execute((reader, writer) -> new Program(reader, writer).run());
+    }
+}
+
+class Shell {
+    void execute(BiConsumer<Scanner, PrintWriter> code) {
+        try (var scanner = new Scanner(System.in); var writer = new PrintWriter(System.out)) {
+            code.accept(scanner, writer);
+        }
+    }
+}
+
+class Program {
+
+    private final Scanner reader;
+    private final PrintWriter writer;
+
+    Program(Scanner reader, PrintWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    void run() {
+        final int r1 = reader.nextInt();
+        final int r2 = reader.nextInt();
+        final int r3 = reader.nextInt();
+        final boolean isFit = r2 + r3 <= r1;
+        writer.println(isFit ? "YES" : "NO");
+    }
+}
---
+++ olymp/acmpru/src/test/java/complexity_from_0_to_10/task_597/MainTest.java
@@ -0,0 +1,30 @@
+package complexity_from_0_to_10.task_597;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import util.MockedStdIO;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static util.Utils.separatedObjectsAsString;
+
+class MainTest {
+
+    @ParameterizedTest
+    @MethodSource("candidates")
+    void testCandidates(String input, String expected) {
+        new MockedStdIO(input).run(output -> {
+            Main.main(new String[]{});
+            assertThat(output.toString()).isEqualTo(expected);
+        });
+    }
+
+    private static Stream<Arguments> candidates() {
+        return Stream.of(
+                Arguments.of(separatedObjectsAsString("10 10 10"), separatedObjectsAsString("NO")),
+                Arguments.of(separatedObjectsAsString("10 3 4"), separatedObjectsAsString("YES"))
+        );
+    }
+}
