914f00cc4e52eefbdaf917e8d36aabcb19aae5e9
Rinat <rinat.zalyaletdinov@yandex.com>
2025-10-02 16:06:52 +0300

    Add new module, resolve a task

---
+++ stepik-courses/algo__algoritmic_tasks_from_interviews/src/main/java/programming_challenges/maximum_pairwise_product/Main.java
@@ -0,0 +1,74 @@
+package programming_challenges.maximum_pairwise_product;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static long getMaxPairwiseProduct(long[] numbers) {
+        int maxIndex1 = 0;
+        int n = numbers.length;
+
+        for (int index = 1; index < n; index++) {
+            if (numbers[index] > numbers[maxIndex1]) {
+                maxIndex1 = index;
+            }
+        }
+
+        int maxIndex2 = maxIndex1 == 0 ? 1 : 0;
+        for (int index = 1; index < n; index++) {
+            if (maxIndex1 != index && numbers[index] > numbers[maxIndex2]) {
+                maxIndex2 = index;
+            }
+        }
+
+        return numbers[maxIndex1] * numbers[maxIndex2];
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        long[] numbers = new long[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextLong();
+        }
+        System.out.println(getMaxPairwiseProduct(numbers));
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new
+                        InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+}
---
+++ stepik-courses/algo__algoritmic_tasks_from_interviews/src/test/java/programming_challenges/maximum_pairwise_product/MainTest.java
@@ -0,0 +1,34 @@
+package programming_challenges.maximum_pairwise_product;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import util.MockedStdIO;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static util.Utils.separatedObjectsAsString;
+
+class MainTest {
+
+    @ParameterizedTest(name = "{index}: input({0}), expected({1})")
+    @MethodSource("candidates")
+    void testCandidates(String input, String expected) {
+        new MockedStdIO(input).run(output -> {
+            Main.main(new String[]{});
+            assertThat(output.toString()).isEqualTo(expected);
+        });
+    }
+
+    private static Stream<Arguments> candidates() {
+        return Stream.of(
+                Arguments.of(separatedObjectsAsString("3", "1 2 3"), separatedObjectsAsString("6")),
+                Arguments.of(separatedObjectsAsString("10", "7 5 14 2 8 8 10 1 2 3"), separatedObjectsAsString("140")),
+                Arguments.of(separatedObjectsAsString("2", "0 1"), separatedObjectsAsString("0")),
+                Arguments.of(separatedObjectsAsString("5", "0 1 20000 20000 10000"), separatedObjectsAsString("400000000")),
+                Arguments.of(separatedObjectsAsString("5", "0 1 10000 20000 10000"), separatedObjectsAsString("200000000")),
+                Arguments.of(separatedObjectsAsString("2", "100000 90000"), separatedObjectsAsString("9000000000"))
+        );
+    }
+}
