97192f4d70a1aad72d390bbed199b408dd787f31
Rinat <rinat.zalyaletdinov@yandex.com>
2025-10-07 14:49:35 +0300

    Resolve acmp 756

---
+++ olymp/acmpru/src/main/java/complexity_from_0_to_10/task_756/Main.java
@@ -0,0 +1,43 @@
+package complexity_from_0_to_10.task_756;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.function.BiConsumer;
+
+/**
+ * <a href="https://acmp.ru/index.asp?main=task&id_task=756">Задача</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Shell().execute((reader, writer) -> new Program(reader, writer).run());
+    }
+}
+
+class Shell {
+    void execute(BiConsumer<Scanner, PrintWriter> code) {
+        try (var scanner = new Scanner(System.in); var writer = new PrintWriter(System.out)) {
+            code.accept(scanner, writer);
+        }
+    }
+}
+
+class Program {
+
+    private final Scanner reader;
+    private final PrintWriter writer;
+
+    Program(Scanner reader, PrintWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    void run() {
+        int m = reader.nextInt();
+        int n = reader.nextInt();
+        if (m == 1 || n == 1) {
+            writer.println(0);
+        } else {
+            writer.println((m * n) - (m + n - 1));
+        }
+    }
+}
---
+++ olymp/acmpru/src/test/java/complexity_from_0_to_10/task_756/MainTest.java
@@ -0,0 +1,32 @@
+package complexity_from_0_to_10.task_756;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import util.MockedStdIO;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static util.Utils.separatedObjectsAsString;
+
+class MainTest {
+
+    @ParameterizedTest(name = "{index}: input({0}), expected({1})")
+    @MethodSource("candidates")
+    void testCandidates(String input, String expected) {
+        new MockedStdIO(input).run(output -> {
+            Main.main(new String[]{});
+            assertThat(output.toString()).isEqualTo(expected);
+        });
+    }
+
+    private static Stream<Arguments> candidates() {
+        return Stream.of(
+                Arguments.of(separatedObjectsAsString("1 1"), separatedObjectsAsString("0")),
+                Arguments.of(separatedObjectsAsString("1 2"), separatedObjectsAsString("0")),
+                Arguments.of(separatedObjectsAsString("2 2"), separatedObjectsAsString("1")),
+                Arguments.of(separatedObjectsAsString("2 3"), separatedObjectsAsString("2"))
+        );
+    }
+}
