1b1076b9f344230b88bccbcd6cff7e27bded063d
Rinat <rinat.zalyaletdinov@yandex.com>
2025-10-09 15:56:04 +0300

    Algorithms: solve last digit of fibonacci

---
+++ stepik-courses/algo__algoritmic_tasks_from_interviews/src/main/java/last_digit_of_fibonacci_number/Main.java
@@ -0,0 +1,27 @@
+package last_digit_of_fibonacci_number;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static int getFibonacciLastDigitNaive(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int prevLastDigit = 0;
+        int currLastDigit = 1;
+        for (int i = 1; i < n; i++) {
+            int tempPrevLastDigit = prevLastDigit;
+            prevLastDigit = currLastDigit;
+            currLastDigit = (tempPrevLastDigit + currLastDigit) % 10;
+        }
+        return currLastDigit;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int c = getFibonacciLastDigitNaive(n);
+        System.out.println(c);
+    }
+}
---
+++ stepik-courses/algo__algoritmic_tasks_from_interviews/src/test/java/last_digit_of_fibonacci_number/MainTest.java
@@ -0,0 +1,31 @@
+package last_digit_of_fibonacci_number;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import util.MockedStdIO;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static util.Utils.separatedObjectsAsString;
+
+class MainTest {
+
+    @ParameterizedTest(name = "{index}: input({0}), expected({1})")
+    @MethodSource("candidates")
+    void testCandidates(String input, String expected) {
+        new MockedStdIO(input).run(output -> {
+            Main.main(new String[]{});
+            assertThat(output.toString()).isEqualTo(expected);
+        });
+    }
+
+    private static Stream<Arguments> candidates() {
+        return Stream.of(
+                Arguments.of(separatedObjectsAsString("3"), separatedObjectsAsString("2")),
+                Arguments.of(separatedObjectsAsString("139"), separatedObjectsAsString("1")),
+                Arguments.of(separatedObjectsAsString("91239"), separatedObjectsAsString("6"))
+        );
+    }
+}
