f760aebad40d57eda8a1c9c4ba837311b6b7999b
Rinat <rinat.zalyaletdinov@yandex.com>
2025-10-11 18:02:09 +0300

    Resolve acmp 839

---
+++ olymp/acmpru/src/main/java/complexity_from_11_to_20/task_839/Main.java
@@ -0,0 +1,38 @@
+package complexity_from_11_to_20.task_839;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.function.BiConsumer;
+
+/**
+ * <a href="https://acmp.ru/index.asp?main=task&id_task=839">Задача</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Shell().execute((reader, writer) -> new Program(reader, writer).run());
+    }
+}
+
+class Shell {
+    void execute(BiConsumer<Scanner, PrintWriter> code) {
+        try (var scanner = new Scanner(System.in); var writer = new PrintWriter(System.out)) {
+            code.accept(scanner, writer);
+        }
+    }
+}
+
+class Program {
+
+    private final Scanner reader;
+    private final PrintWriter writer;
+
+    Program(Scanner reader, PrintWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    void run() {
+        final String survey = reader.next();
+        writer.println(survey.indexOf('0') != -1 ? "NO" : "YES");
+    }
+}
---
+++ olymp/acmpru/src/test/java/complexity_from_11_to_20/task_839/MainTest.java
@@ -0,0 +1,30 @@
+package complexity_from_11_to_20.task_839;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import util.MockedStdIO;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static util.Utils.separatedObjectsAsString;
+
+class MainTest {
+
+    @ParameterizedTest(name = "{index}: input({0}), expected({1})")
+    @MethodSource("candidates")
+    void testCandidates(String input, String expected) {
+        new MockedStdIO(input).run(output -> {
+            Main.main(new String[]{});
+            assertThat(output.toString()).isEqualTo(expected);
+        });
+    }
+
+    private static Stream<Arguments> candidates() {
+        return Stream.of(
+                Arguments.of(separatedObjectsAsString("11111101010011"), separatedObjectsAsString("NO")),
+                Arguments.of(separatedObjectsAsString("11"), separatedObjectsAsString("YES"))
+        );
+    }
+}
