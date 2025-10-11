b41c0817666e5273fa0d2cfa492d68c49ac70e7e
Rinat <rinat.zalyaletdinov@yandex.com>
2025-10-11 18:18:32 +0300

    Resolve acmp 514

---
+++ olymp/acmpru/src/main/java/complexity_from_11_to_20/task_514/Main.java
@@ -0,0 +1,43 @@
+package complexity_from_11_to_20.task_514;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.function.BiConsumer;
+
+/**
+ * <a href="https://acmp.ru/index.asp?main=task&id_task=514">Задача</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Shell().execute((reader, writer) -> new Program(reader, writer).run());
+    }
+}
+
+class Shell {
+    void execute(BiConsumer<Scanner, PrintWriter> code) {
+        try (var scanner = new Scanner(System.in); var writer = new PrintWriter(System.out)) {
+            code.accept(scanner, writer);
+        }
+    }
+}
+
+class Program {
+
+    private final Scanner reader;
+    private final PrintWriter writer;
+
+    Program(Scanner reader, PrintWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    void run() {
+        int k = reader.nextInt();
+        int n = 0;
+        while (n < k) {
+            n++;
+            k -= n;
+        }
+        writer.println(n);
+    }
+}
