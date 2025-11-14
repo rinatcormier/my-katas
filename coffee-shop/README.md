13530c10685e15abeab67ea03c7de2e0c8c04a2b
Rinat <rinat.zalyaletdinov@yandex.com>
2025-11-14 12:10:09 +0300

    Add sources of course Containerized-Java-EE-8-Apps-using-Docker-and-Kubernetes
    See: https://github.com/PacktPublishing/Containerized-Java-EE-8-Apps-using-Docker-and-Kubernetes

---
+++ coffee-shop/README.md
@@ -0,0 +1,4 @@
+Sources from course [Developing-Containerized-Java-EE-8-Apps-using-Docker-and-Kubernetes](https://github.com/PacktPublishing/Containerized-Java-EE-8-Apps-using-Docker-and-Kubernetes).
+
+Supposed to build `./openliberty/Dockerfile` as a base image
+then `./Dockerfile`.
---
+++ coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Configurator.java
@@ -0,0 +1,42 @@
+package com.sebastian_daschner.coffee_shop;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@ApplicationScoped
+public class Configurator {
+
+    private final Properties properties = new Properties();
+
+    @PostConstruct
+    private void initProperties() {
+        try (final InputStream appConfig = new FileInputStream("config/application.properties");
+             final InputStream credentialsConfig = new FileInputStream("config/credentials.properties")) {
+            properties.load(appConfig);
+            properties.load(credentialsConfig);
+        } catch (IOException e) {
+            System.out.println("============================");
+            e.printStackTrace();
+            System.out.println("============================");
+            System.out.println(System.getProperty("user.dir"));
+            System.out.println("============================");
+            throw new RuntimeException("Could not init configuration", e);
+        }
+    }
+
+    @Produces
+    @Config("unused")
+    public String exposeConfig(InjectionPoint injectionPoint) {
+        final Config config = injectionPoint.getAnnotated().getAnnotation(Config.class);
+        if (config != null)
+            return properties.getProperty(config.value());
+        return null;
+    }
+
+}
